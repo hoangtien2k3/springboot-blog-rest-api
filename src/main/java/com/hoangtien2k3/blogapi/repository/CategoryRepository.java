8732d0c87ace3463a22570dc9cd8c08d72062925
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-08-12 20:33:41 +0700

    te

--- src/main/java/com/hoangtien2k3/blogapi/repository/CategoryRepository.java
+++ src/main/java/com/hoangtien2k3/blogapi/repository/CategoryRepository.java
@@ -4,4 +4,7 @@ import com.hoangtien2k3.blogapi.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
 }
